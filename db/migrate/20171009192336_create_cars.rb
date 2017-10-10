b21320e2808e5db9c7e7ead79d42ac1e4c8ba949
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-09 19:29:53 -0700

    sets up migrations for the cars and the users classes

--- db/migrate/20171009192336_create_cars.rb
+++ db/migrate/20171009192336_create_cars.rb
@@ -1,4 +1,13 @@
 class CreateCars < ActiveRecord::Migration
   def change
+    create_table :cars do |t|
+      t.string :make, null: false
+      t.string :model, null: false
+      t.string :type
+      t.integer :top_speed
+      t.integer :horsepower
+
+      t.timestamps
+    end
   end
 end
